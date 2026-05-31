6745b48eb3458431e00bc39f33f89ee8bd662fe2
Gabriel Hansmann <gabrielhansmann@web.de>
2026-05-31 14:08:55 +0200

    [Refactor] ability for cubemap texture to map textures onto spheres

--- src/compute_pass.cpp
+++ src/compute_pass.cpp
@@ -2,16 +2,51 @@
 #include "utils/shaders.h"
 #include <iostream>
 
-ComputePass::ComputePass(int w, int h, const char* shaderPath): w_(w), h_(h) {
-	program_ = LoadComputeShader(shaderPath);
+ComputePass::ComputePass(
+	int w,
+	int h,
+	const char* shaderPath,
+	ComputePassTextureType textureType,
+	const void* const* cubeFaces,
+	const std::string& defines
+): shaderPath_(shaderPath), w_(w), h_(h), textureType_(textureType) {
+	program_ = LoadComputeShaderWithDefines(shaderPath, defines);
 
 	glGenTextures(1, &tex_);
-	glBindTexture(GL_TEXTURE_2D, tex_);
-	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA32F, w, h, 0, GL_RGBA, GL_FLOAT, nullptr);
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+	if (textureType_ == ComputePassTextureType::CubeMap) {
+		glBindTexture(GL_TEXTURE_CUBE_MAP, tex_);
+		glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+		glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+		glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_WRAP_R, GL_CLAMP_TO_EDGE);
+		glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+		glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+		glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_BASE_LEVEL, 0);
+		glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_MAX_LEVEL, 0);
+
+		imageFormat_ = GL_RGBA8;
+		for (int face = 0; face < 6; ++face) {
+			const void* facePixels = cubeFaces ? cubeFaces[face] : nullptr;
+			glTexImage2D(
+				GL_TEXTURE_CUBE_MAP_POSITIVE_X + face,
+				0,
+				imageFormat_,
+				w_,
+				h_,
+				0,
+				GL_BGRA,
+				GL_UNSIGNED_BYTE,
+				facePixels
+			);
+		}
+	} else {
+		glBindTexture(GL_TEXTURE_2D, tex_);
+		imageFormat_ = GL_RGBA32F;
+		glTexImage2D(GL_TEXTURE_2D, 0, imageFormat_, w_, h_, 0, GL_RGBA, GL_FLOAT, nullptr);
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+	}
 
 }
 
@@ -22,15 +57,28 @@ ComputePass::~ComputePass() {
 
 void ComputePass::markDirty() { dirty_ = true; }
 
+void ComputePass::reloadProgram(const std::string& defines) {
+	GLuint newProgram = LoadComputeShaderWithDefines(shaderPath_.c_str(), defines);
+	if (program_)
+		glDeleteProgram(program_);
+	program_ = newProgram;
+	dirty_ = true;
+}
+
 void ComputePass::dispatch() {
 	if (!dirty_) return;
 
 	glUseProgram(program_);
-	glBindImageTexture(0, tex_, 0, GL_FALSE, 0, GL_WRITE_ONLY, GL_RGBA32F);
 	// a grid of 8x8 texel work groups covering the entire texture. Calling division ensures
 	// we cover every texel when size isnt a multiple of 8
 	// the shaders bounds check discards invocations that land past the edge
-	glDispatchCompute((w_ + 7) / 8, (h_ + 7) / 8, 1);
+	if (textureType_ == ComputePassTextureType::CubeMap) {
+		glBindImageTexture(0, tex_, 0, GL_TRUE, 0, GL_WRITE_ONLY, imageFormat_);
+		glDispatchCompute((w_ + 7) / 8, (h_ + 7) / 8, 6);
+	} else {
+		glBindImageTexture(0, tex_, 0, GL_FALSE, 0, GL_WRITE_ONLY, imageFormat_);
+		glDispatchCompute((w_ + 7) / 8, (h_ + 7) / 8, 1);
+	}
 	glMemoryBarrier(GL_TEXTURE_FETCH_BARRIER_BIT | GL_SHADER_IMAGE_ACCESS_BARRIER_BIT);
 
 	dirty_ = false;
--- src/compute_pass.h
+++ src/compute_pass.h
@@ -1,22 +1,39 @@
 #pragma once
 #include <glad/glad.h>
+#include <string>
 
 // introduce ComputePass to work only if inputs change
+enum class ComputePassTextureType {
+	Texture2D,
+	CubeMap
+};
+
 class ComputePass {
 	public:
-		ComputePass(int w, int h, const char* shaderPath);
+		ComputePass(
+			int w,
+			int h,
+			const char* shaderPath,
+			ComputePassTextureType textureType = ComputePassTextureType::Texture2D,
+			const void* const* cubeFaces = nullptr,
+			const std::string& defines = std::string()
+		);
 		~ComputePass();
 
 		// GPU work is done when dirty flag is set true
 		// dispatch() checks the flag and does GPU work
 		void markDirty();
 		void dispatch();
+		void reloadProgram(const std::string& defines);
 		GLuint texture() const { 
 			return tex_; 
 		}
 	private:
+		std::string shaderPath_;
 		GLuint program_;
 		GLuint tex_;
 		int w_, h_;
+		ComputePassTextureType textureType_ = ComputePassTextureType::Texture2D;
+		GLenum imageFormat_ = GL_RGBA32F;
 		bool dirty_ = true;
 };
